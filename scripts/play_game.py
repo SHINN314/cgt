80e735f4512d83c344c23299c46b69120d4466fd
SHINN314 <shinnosukekimura8@gmail.com>
2025-10-24 03:34:14 +0900

    update: playerの勝敗を明示するようにした

--- scripts/play_game.py
+++ scripts/play_game.py
@@ -6,9 +6,20 @@ def play_game() -> None:
     row: int = int(input("行数を入力してください: "))
     col: int = int(input("列数を入力してください: "))
     game = Chomp(row, col)
+    is_first_player: int = 1
 
     while game.is_game_over() is False:
         game.display()
         r: int = int(input("食べる行のインデックスを入力してください: "))
         c: int = int(input("食べる列のインデックスを入力してください: "))
         game.eat(r, c)
+        is_first_player ^= 1
+
+    if is_first_player:
+        print("第二プレイヤーの勝ちです!")
+    else:
+        print("第一プレイヤーの勝ちです!")
+
+
+if __name__ == "__main__":
+    play_game()
